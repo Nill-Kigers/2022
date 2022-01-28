70d9995170f6df4928f2f85ac388b6e895b2d290
Your Name <you@example.com>
2022-01-28 14:43:33 +0300

    Init

---
+++ school/Pages/AddService.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace school
+{
+    /// <summary>
+    /// Логика взаимодействия для AddService.xaml
+    /// </summary>
+    public partial class AddService : Page
+    {
+        public OpenFileDialog ofd = new OpenFileDialog();
+        string path = "";
+        private bool flag = false;
+        private string _imgSource = string.Empty;
+        private Service _selectService = new Service();
+        public AddService(Service selectService)
+        {
+            InitializeComponent();
+
+            if (selectService != null)
+                _selectService = selectService;
+            DataContext = _selectService;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (flag)
+            {
+                File.Copy(ofd.FileName, _imgSource, true);
+                _selectService.MainImagePath = $"\\Услуги школы\\{ofd.SafeFileName}";
+            }
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_selectService.Title))
+                errors.AppendLine("Укажите название");
+            if (string.IsNullOrWhiteSpace(_selectService.ToString()))
+                errors.AppendLine("Укажите цену");
+            if (_selectService.Cost == null)
+                errors.AppendLine("Укажите Длительность в секундах");
+            if (_selectService.DurationInSeconds == null)
+                errors.AppendLine("Укажите Описание");
+            if (string.IsNullOrWhiteSpace(_selectService.Description))
+                errors.AppendLine("Укажите КПП");
+            if (_selectService.Discount == null)
+                errors.AppendLine("Укажите скидку");
+            
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_selectService.ID == 0)
+                schoolEntities.GetContext().Services.Add(_selectService);
+            try
+            {
+                schoolEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                FrameWindow.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void SelectedPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            string Source = Environment.CurrentDirectory;
+            if (ofd.ShowDialog() == true)
+            {
+                flag = true;
+                string ing = ofd.SafeFileName;
+                _imgSource = Source.Replace("\\bin\\Debug", "\\Услуги школы\\") + ing;
+                PreviewImage.Source = new BitmapImage(new Uri(ofd.FileName));
+                path = ofd.FileName;
+            }
+        }
+    }
+}
---
+++ school/Pages/DataLern.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace school.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DataLern.xaml
+    /// </summary>
+    public partial class DataLern : Page
+    {
+        public DataLern()
+        {
+            InitializeComponent();
+            ListView.ItemsSource = schoolEntities.GetContext().Services.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            FrameWindow.MainFrame.Navigate(new AddService((sender as Button).DataContext as Service));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update((SortBox.SelectedItem as ComboBoxItem).Content.ToString(), TypeBox.Text);
+        }
+
+        private void TypeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update(SortBox.Text, (TypeBox.SelectedItem as string).ToString());
+        }
+
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update(SortBox.Text, TypeBox.Text, SearchBox.Text);
+            if (ListView.Items.Count == 0)
+            {
+                MessageBox.Show("Ничего не найдено");
+            }
+        }
+
+        public void Update(string sort = "", string filt = "", string searh = "")
+        {
+            var data = schoolEntities.GetContext().Services.ToList();
+            if (!string.IsNullOrEmpty(searh) && !string.IsNullOrWhiteSpace(searh))
+            {
+                data = data.Where(p => p.Title.ToLower().Contains(searh.ToLower())).ToList();
+            }
+            if (!string.IsNullOrEmpty(filt) && !string.IsNullOrWhiteSpace(filt))
+            {
+                if (filt != "Все")
+                {
+                    if (filt == "Скидка (0% - 15%)")
+                    {
+                        
+                        
+                    }    
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(sort) && !string.IsNullOrEmpty(sort))
+            {
+                if (sort == "Стоимость (по возрастанию)")
+                {
+                    data = data.OrderBy(p => p.Cost).ToList();
+                }
+                if (sort == "Стоимость (по убыванию)")
+                {
+                    data = data.OrderByDescending(p => p.Cost).ToList();
+                }
+                if (sort == "Размер скидки (по возрастанию)")
+                {
+                    data = data.OrderBy(p => p.Discount).ToList();
+                }
+                if (sort == "Размер скидки (по убыванию)")
+                {
+                    data = data.OrderByDescending(p => p.Discount).ToList();
+                }
+            }
+            ListView.ItemsSource = data;
+        }
+    }
+}
